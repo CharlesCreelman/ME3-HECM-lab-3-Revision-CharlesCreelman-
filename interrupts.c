ca960d77f1dab08a1d9a1ede8f570b97b722246d
CharlesCreelman <charles.creelman@gmail.com>
2021-11-07 07:47:19 +0000

    Cloned and reverted to uneditted version

--- interrupts.c
+++ interrupts.c
@@ -7,14 +7,7 @@
 ************************************/
 void Interrupts_init(void)
 {
-    
-    PIE0bits.INT0IE=1; 	//enable interrupt source INT0
-    PIE2bits.C1IE=1;    // enables comparator interrupt
-    INTCONbits.GIE=1; 	//turn on interrupts globally (when this is off, all interrupts are deactivated)
-    INTCONbits.IPEN=1;  //enables interrupt priority feature
-    PIE0bits.TMR0IE =1; //enable the timer interrupt
-    IPR0bits.TMR0IP=1;
-    // turn on global interrupts, peripheral interrupts and the interrupt source 
+	// turn on global interrupts, peripheral interrupts and the interrupt source 
 	// It's a good idea to turn on global interrupts last, once all other interrupt configuration is done.
 }
 
@@ -24,20 +17,6 @@ void Interrupts_init(void)
 ************************************/
 void __interrupt(high_priority) HighISR()
 {
-	if(PIR2bits.C1IF){ 					//check the interrupt source
-	LATDbits.LATD7 = 1;
-	PIR2bits.C1IF=0; 						//clear the interrupt flag!
-	__delay_ms(500);
-    LATDbits.LATD7 = 0;
-    }	
-    //add your ISR code here i.e. check the flag, do something (i.e. toggle an LED), clear the flag...
-    if(PIR0bits.TMR0IF){
-        LATHbits.LATH3 = !LATHbits.LATH3;//changes the value of the right LED
-        PIR0bits.TMR0IF=0; //Resets the flag
-    }
-    
-    //HOW DO I GET THE INTERRUPT TO WORK ABOVE
+	//add your ISR code here i.e. check the flag, do something (i.e. toggle an LED), clear the flag...
 }
-
-
 
--- main.c
+++ main.c
@@ -9,29 +9,14 @@
 #include "LEDarray.h"
 #include "interrupts.h"
 #include "comparator.h"
-#include "timers.h"
 
 #define _XTAL_FREQ 64000000 //note intrinsic _delay function is 62.5ns at 64,000,000Hz  
 
-// Exercise 1
-
-//void main(void) {
-//	//call your initialisation functions to set up the hardware modules
-//    Interrupts_init();
-//    Comp1_init();
-//    TRISDbits.TRISD7=0; // This sets the pin to be an output
-//    while (1) {
-//        
-//		Sleep();
-//    }
-//}
 
 void main(void) {
-    Timer0_init();
-    LEDarray_init();
-    Interrupt_init();
-    while(1) {
-        //unsigned int counter = T0CON0bits.T0OUT;
-        LEDarray_disp_bin(get16bitTMR0val());
+	//call your initialisation functions to set up the hardware modules
+
+    while (1) {
+		
     }
 }
